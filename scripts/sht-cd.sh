f8ccad7141703771beecae6f8d4ff03bc6ef644e
nin0dev <personal@nin0.dev>
2025-03-03 21:08:38 -0500

    scripts: just cd in dir bro

--- scripts/sht-cd.sh
+++ scripts/sht-cd.sh
@@ -3,5 +3,6 @@ if [ -z "stacks/$1" ]; then
   echo "what"
   return 1
 fi
-echo "You are now in the $1 directory in an ephemeral shell."
-bash -c "cd stacks/$1; exec bash"
+echo "You are now in the $1 directory."
+cd stacks/$1
+pwd

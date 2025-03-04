4d4552f0cb693ba3228914236ff6d5fab0365288
nin0dev <personal@nin0.dev>
2025-03-03 20:39:30 -0500

    scripts: add funny commands

---
+++ scripts/sht-env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ -f "stacks/$1/.env.example" ] && [ ! -f "stacks/$1/.env" ]; then
+  cp "stacks/$1/.env.example" "stacks/$1/.env"
+elif [ ! -f "stacks/$1/.env.example" ]; then
+  echo "what"
+  exit 1
+fi
+${EDITOR:-nano} "stacks/$1/.env"
---
+++ scripts/sht-stack.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+${EDITOR:-nano} "stacks/$1/compose.yaml"

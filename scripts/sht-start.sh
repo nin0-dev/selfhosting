f8c5aafe429e84e12a63e5361ba320d21d8442e9
nin0dev <personal@nin0.dev>
2025-03-03 19:47:25 -0500

    scripts: make start create bind mounts as nonroot

--- scripts/sht-start.sh
+++ scripts/sht-start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 _() {
-  cd stacks/$1
-  docker compose up -d
+    cd stacks/$1
+    volumes=$(yq e '.services[].volumes[]' compose.yaml | grep -oP '^\S+(?=:)' | sort -u) # i love AI
+    for volume in $volumes; do
+        mkdir -p "$volume"
+    done
+    docker compose up -d
 } && _ "$@"
 unset -f _

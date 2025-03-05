b27a08f0205cb0a26f0f2a3c69169c22a2216aea
nin0dev <personal@nin0.dev>
2025-03-05 14:04:16 -0500

    scripts/backup: add

---
+++ scripts/sht-backup.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+yap() {
+    echo -e "\033[1;33m>>> $@ <<<\033[0m"
+}
+
+# Checks
+if [[ $EUID -ne 0 ]]; then
+    yap "elevating..."
+    sudo "$0" "$@"
+    exit $?
+fi
+basedir=$(basename "$(pwd)")
+if [[ "$basedir" != "sht" && "$basedir" != "selfhosting" ]]; then
+    echo "go to the base selfhosting root dir"
+    exit 1
+fi
+
+# Prepare
+working_dir="$(hostname)-backup-$(date +'%d-%m-%Y-%H-%M-%S')"
+yap creating backup $working_dir
+mkdir $working_dir
+
+# Container data backup (raw)
+yap pausing containers
+for stack in stacks/*; do
+    docker compose -f $stack/compose.yaml pause &> /dev/null
+done
+yap packing container data
+tar -cf $working_dir/data.tar data
+yap unpausing containers
+for stack in stacks/*; do
+    docker compose -f $stack/compose.yaml unpause &> /dev/null
+done
+
+# Host postgres backup
+yap backing up host postgres
+sudo -u postgres pg_dumpall > $working_dir/postgres-host-backup.sql
+
+# Container postgres backup
+for container in $(docker ps --format '{{.Names}}' | grep db | awk '{print $1}'); do
+    yap backing up postgres in container $container
+    docker exec $container pg_dumpall -U postgres > $working_dir/postgres-$container-backup.sql
+done
+
+# Compress and encrypt backup
+yap compressing backup...
+tar -czf $working_dir.tgz $working_dir
+chown nin0:nin0 $working_dir.tgz
+
+# Upload backup
+yap uploading backup...
+sudo -u nin0 rclone copy $working_dir.tgz iCloud:Backup/
+
+# Clean up
+yap done!
+if [[ "$basedir" == "selfhosting" ]]; then
+    sleep 6
+fi
+rm -rf $working_dir*
